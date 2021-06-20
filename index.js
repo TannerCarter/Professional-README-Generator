4c0c027139d64b08f68f5aeb8c0a0691925942ef
Tanner C <tannerc.bootcamp@gmail.com>
2021-06-20 04:51:25 -0700

    Required fs and generateMarkdown file, package.json created, need to install inquirer

---
+++ index.js
@@ -0,0 +1,19 @@
+// TODO: Include packages needed for this application
+
+//require fs
+const fs = require("fs");
+//require generateMarkdown.js file
+const generateMarkdown = require("./utils/generateMarkdown.js");
+//Add inquirer below after npm install
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
