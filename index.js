7b8fe0a3600b201ba81abe2eb2b3bc3ed8b2706d
Tanner C <tannerc.bootcamp@gmail.com>
2021-06-20 19:46:29 -0700

    Added questions, added license functions, added badge functions

--- index.js
+++ index.js
@@ -7,7 +7,199 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 //require inquirer
 const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  //Add to the section enititled Description, Installation, Usage, Contributing, and Tests - Start
+
+  //Title
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?(Required)",
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log("You must enter the title of your project!");
+        return false;
+      }
+    },
+  },
+  //Description
+  {
+    type: "input",
+    name: "description",
+    message:
+      "How would you describe your project? Please enter a description.(Required)",
+    validate: (descriptionInput) => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log("You must enter a description of your project!");
+        return false;
+      }
+    },
+  },
+  //Installation
+  {
+    type: "input",
+    name: "installaton",
+    message: "What are the installation instructions for your project?",
+    validate: (intallationInput) => {
+      if (intallationInput) {
+        return true;
+      } else {
+        console.log(
+          "You must provide installation instructions for your project!"
+        );
+        return false;
+      }
+    },
+  },
+  //Credits
+  {
+    type: "input",
+    name: "credit",
+    message: "Who would you like to credit on this project?",
+    validate: (creditInput) => {
+      if (creditInput) {
+        return true;
+      } else {
+        console.log("You must provide credit to other devs for your project!");
+        return false;
+      }
+    },
+  },
+  //Usage
+  {
+    type: "input",
+    name: "usage",
+    message: "What are the instructions for the use of your project?",
+    validate: (usageInput) => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log(
+          "You must provide instructions for the use of your project!"
+        );
+        return false;
+      }
+    },
+  },
+  //Contributing
+  {
+    type: "input",
+    name: "contributing",
+    message:
+      "What are your guidelines for other developers to contribute your application or package?",
+    validate: (contributingInput) => {
+      if (contributingInput) {
+        return true;
+      } else {
+        console.log("You must provide your contribution guidelines!");
+        return false;
+      }
+    },
+  },
+  // Add to the section enititled Description, Installation, Usage, Contributing, and Tests - End
+
+  //Add to the section enititled License - Start
+  //License
+  {
+    type: "list",
+    name: "license",
+    message: "Which license from this list applies to your project:",
+    choices: [
+      "No license",
+      "Apache License 2.0",
+      "Boost Software License 1.0",
+      "GNU AGPLv3",
+      "GNU GPLv3",
+      "GNU LGPLv3",
+      "MIT",
+      "Mozilla Public License 2.0",
+      "The Unlicense",
+    ],
+  },
+  //Add to the section enititled License - End
+
+  //Add to the section entitled Badges - Start
+  //Badges
+  {
+    type: "list",
+    name: "badges",
+    message:
+      "Which language from this list is applied the most in your project:",
+    choices: ["Javascript", "HTML", "CSS", "Node JS", "No badges"],
+  },
+  //Add to the section entitled Badges - End
+
+  //Add to the section entitled Features - Start
+  //Features
+  {
+    type: "input",
+    name: "features",
+    message: "What features are in your project?",
+    validate: (featuresInput) => {
+      if (featuresInput) {
+        return true;
+      } else {
+        console.log("You must provide the features in your project!");
+        return false;
+      }
+    },
+  },
+  //Add to the section entitled Features - End
+
+  //Add to the section entitled Tests - Start
+  //Tests
+  {
+    type: "input",
+    name: "tests",
+    message: "What tests can you run for your project(Include Examples)?",
+    validate: (testsInput) => {
+      if (testsInput) {
+        return true;
+      } else {
+        console.log(
+          "You must provide how to test your project, with examples!"
+        );
+        return false;
+      }
+    },
+  },
+  //Add to the section entitled Tests - End
+
+  //Add to the section enititled Questions - Start
+  //Github Username
+  {
+    type: "input",
+    name: "githubUsername",
+    message: "What is your Github username?(Required)",
+    validate: (githubUsernameInput) => {
+      if (githubUsernameInput) {
+        return true;
+      } else {
+        console.log("You must provide your Github username!");
+        return false;
+      }
+    },
+  },
+  //Email Address
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email address?",
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log("You must provide your email address!");
+        return false;
+      }
+    },
+  },
+  //Add to the section enititled Questions - End
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,36 +1,193 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderlicenseBadge(license) {
+  if (license != "No license") {
+    var cert;
+    switch (license) {
+      case "Apache license 2.0":
+        cert = "Apache%20license%202.0";
+        break;
+      case "Boost Software license 1.0":
+        cert = "Boost%20Software%20license%201.0";
+        break;
+      case "GNU AGPLv3":
+        cert = "GNU%20AGPLv3";
+        break;
+      case "GNU GPLv3":
+        cert = "GNU%20GPLv3";
+        break;
+      case "GNU LGPLv3":
+        cert = "GNU%20LGPLv3";
+        break;
+      case "MIT":
+        cert = "MIT";
+        break;
+      case "Mozilla Public license 2.0":
+        cert = "Mozilla%20Public%20license%202.0";
+        break;
+      case "The Unlicense":
+        cert = "unlicense";
+        break;
+    }
+    return `![license badge](https://img.shields.io/badge/license-${cert}-brightgreen)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderlicenseLink(license) {
+  if (license != "No license") {
+    var cert;
+    switch (license) {
+      case "Apache License 2.0":
+        cert = "apache-2.0";
+        break;
+      case "Boost Software License 1.0":
+        cert = "bsl-1.0";
+        break;
+      case "GNU AGPLv3":
+        cert = "agpl-3.0";
+        break;
+      case "GNU GPLv3":
+        cert = "gpl-3.0";
+        break;
+      case "GNU LGPLv3":
+        cert = "lgpl-3.0";
+        break;
+      case "MIT":
+        cert = "mit";
+        break;
+      case "Mozilla Public License 2.0":
+        cert = "mpl-2.0";
+        break;
+      case "The Unlicense":
+        cert = "unlicense";
+        break;
+    }
+    return `![License badge]https://choosealicense.com/licenses/${cert}/`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderlicenseSection(license) {
+  if (license != "No license") {
+    return `
+  # license 
+  ${renderlicenseLink(license)}`;
+  }
+  return "";
+}
+
+// TODO: Create a function that returns a badge based on which language is passed in
+// If there is no badge, return an empty string
+function renderBadges(badges) {
+  if (badges != "No badges") {
+    var lang;
+    switch (badges) {
+      case "Javascript":
+        lang = "Javascript";
+        break;
+      case "HTML":
+        lang = "HTML";
+        break;
+      case "CSS":
+        lang = "CSS";
+        break;
+      case "Node JS":
+        lang = "Node%20JS";
+        break;
+    }
+    return `![Language Badge]https://img.shields.io/badge/Language-${lang}-brightgreen`;
+  }
+  return "";
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderbadgeLink(badges) {
+  if (badges != "No badges") {
+    var lang;
+    switch (badges) {
+      case "Javascript":
+        lang = "Javascript";
+        break;
+      case "HTML":
+        lang = "HTML";
+        break;
+      case "CSS":
+        lang = "CSS";
+        break;
+      case "Node JS":
+        lang = "Node%20JS";
+        break;
+    }
+    return `![Language Badge]https://img.shields.io/badge/Language-${lang}-brightgreen`;
+  }
+  return "";
+}
+
+// TODO: Create a function that returns the badge section of README
+// If there is no badges, return an empty string
+function renderbadgeSection(badges) {
+  if (badges != "No Badges") {
+    return `
+  # Badges 
+  ${renderbadgeLink(badges)}`;
+  }
+  return "";
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title} 
+  \n 
+  ${renderlicenseBadge(data.license)} 
+  \n 
+  ${renderBadges(data.badges)}
 
   # Description
+  ${data.description}
 
   # Table of Contents
-
+  * [Installation](#installation)
+  * [Credit](#credit)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [license](#license)
+  * [Badges](#badges)
+  * [Features](#features)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
   # Installation
+  ${data.installation}
+
+  # Usage
+  ${data.usage}
 
   # Credits
+  ${data.credit}
 
-  # License
+  ${renderlicenseSection(data.license)}
 
-  # Badges
+  ${renderbadgeSection(data.badge)}
+
 
   # Features
+  ${data.features}
 
   # Contributing
+  ${data.contributing}
 
   # Tests
+  ${data.tests}
+
+  # Questions
+  Github: https://github.com/${data.githubUsername}
+  For any questions or concerns, please reach out to me via email: ${data.email}
 
 `;
 }
